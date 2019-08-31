e381e79210fc446f116fffd630940aa47048740a
garryfang <garrybest@foxmail.com>
2019-08-31 11:15:27 +0800

    add q4

---
+++ src/q4.cpp
@@ -0,0 +1,25 @@
+/*
+ * 二维数组中的查找
+ * @Author: garryfang 
+ * @Date: 2019-08-31 11:00:31 
+ * @Last Modified by: garryfang
+ * @Last Modified time: 2019-08-31 11:10:27
+ */
+#include <vector>
+
+bool find(std::vector<std::vector<int>> &matrix, int number)
+{
+    if (matrix.empty() || matrix[0].empty())
+        return false;
+    int i(0), j(matrix[0].size() - 1);
+    while (i < matrix.size() && j >= 0)
+    {
+        if (matrix[i][j] == number)
+            return true;
+        else if (matrix[i][j] > number)
+            --j;
+        else
+            ++i;
+    }
+    return false;
+}
