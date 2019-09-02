e3b6b3a624c92e7946b80b3e43e67527a1da763b
garryfang <garrybest@foxmail.com>
2019-09-02 22:08:17 +0800

    add q28

---
+++ src/q28.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Author: garryfang 
+ * @Date: 2019-09-02 21:59:19 
+ * @Last Modified by: garryfang
+ * @Last Modified time: 2019-09-02 22:07:43
+ */
+
+#include "BinaryTreeNode.h"
+
+bool isSymmetrical(BinaryTreeNode *left, BinaryTreeNode *right)
+{
+    if (!left && !right)
+        return true;
+    if (!left || !right || left->value != right->value)
+        return false;
+    return isSymmetrical(left->left, right->right) && isSymmetrical(left->right, right->left);
+}
+
+bool isSymmetrical(BinaryTreeNode *root)
+{
+    return isSymmetrical(root, root);
+}
