1a8fa1b66baec2676d2f251bbf34c5a051269772
garryfang <garrybest@foxmail.com>
2019-09-18 19:58:25 +0800

    add q65

---
+++ src/q65.cpp
@@ -0,0 +1,19 @@
+/*
+ * 不用加减乘除做加法
+ * @Author: garryfang 
+ * @Date: 2019-09-18 19:39:56 
+ * @Last Modified by: garryfang
+ * @Last Modified time: 2019-09-18 19:57:22
+ */
+
+int add(int num1, int num2)
+{
+    int sum, carry;
+    do
+    {
+        sum = num1 ^ num2;
+        carry = (num1 & num2) << 1;
+        num1 = sum;
+        num2 = carry;
+    } while (num2 != 0);
+}
